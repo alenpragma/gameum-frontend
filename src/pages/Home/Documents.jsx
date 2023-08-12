a45ed2daeb2a7ecd6ce7548855483ecf169a7f5e
Remon Tripura <remontripura045@gmail.com>
2023-08-12 12:20:49 +0600

    pdf download problem fixed

--- src/pages/Home/Documents.jsx
+++ src/pages/Home/Documents.jsx
@@ -5,8 +5,7 @@ import { GoCheckbox } from "react-icons/go";
 import { RxPencil2 } from "react-icons/rx";
 import { FaCartShopping } from "react-icons/fa6";
 import Heading from '../../components/Heading';
-import DownloadPdf from '../../components/DownloadPdf';
-import pdf from "../../assets/gameum.pdf"
+import pdf from "../../assets/gameum.pdf";
 
 const Documents = () => {
     return (
@@ -25,7 +24,8 @@ const Documents = () => {
                                 <p>White Paper</p>
                             </div>
                             <div className="mt-16 ">
-                                <DownloadPdf pdfUrl={pdf} />
+                                {/* <DownloadPdf pdfUrl={pdf} /> */}
+                                <button className="text-2xl font-semibold relative uppercase button-details" onClick={() => window.open(`${pdf}`, '_blank')}>Download</button>
                             </div>
                         </div>
                     </div>
